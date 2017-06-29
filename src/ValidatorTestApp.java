d66481ac4880d7162fa6831732756093cd34e9af
Matthew Menna <mattmenna@gmail.com>
2017-06-29 09:51:08 -0400

    Initial Commit

---
+++ src/ValidatorTestApp.java
@@ -0,0 +1,19 @@
+
+/*
+GCBonus4
+ValidatorTestApp
+Written by Matthew MEnna
+6/29/2017
+9:48 AM
+2017
+IntelliJ IDEA
+Used to tet the methods in the validator, OOValidator, and MyValidator classes.
+ */
+
+
+public class ValidatorTestApp {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
