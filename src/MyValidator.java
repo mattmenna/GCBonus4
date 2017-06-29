1184dda72bf4360245fb1b8b38fa69ff44a4c1e3
Matthew Menna <mattmenna@gmail.com>
2017-06-28 21:56:00 -0400

    Initial Commit

--- src/MyValidator.java
+++ src/MyValidator.java
@@ -1,7 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 /**
  * Created by Matt on 6/28/2017.
  */
-public class MyValidator {
+public class MyValidator extends OOValidator {
+
+    public MyValidator(Scanner sc) {
+        super(sc);
+    }
+
+    public String getRequiredString(String prompt) {
+        String rString = "";
+        return rString;
+    }
+
+    public String getChoiceString(String prompt, String s1, String s2) {
+        String rString = "";
+        return rString;
+    }
 }
--- src/OOValidator.java
+++ src/OOValidator.java
@@ -5,23 +5,31 @@ import java.util.Scanner;
  */
 public class OOValidator {
 
+    Scanner sc;
+
     public OOValidator(Scanner sc) {
 
     }
 
     public int getInt(String prompt) {
+        System.out.println("Enter any integer:");
+        sc.nextLine()
+
         return 0;
     }
 
     public int getIntWithinRange(String prompt, int min, int max){
+        System.out.println("Enter any integer between " + min + "and " + max + ":");
         return 0;
     }
 
     public double getDouble(String prompt){
+        System.out.println("Enter any double:");
         return 0;
     }
 
     public double getDoubleWithinRange(String prompt, double min, double max){
+        System.out.println("Enter any double between " + min + "and " + max + ":");
         return 0;
     }
 }
