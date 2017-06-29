267b19e86c920d68f5843d3f0ffe144caeb701de
Matthew Menna <mattmenna@gmail.com>
2017-06-28 21:41:19 -0400

    Initial commit

---
+++ src/MyValidator.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Matt on 6/28/2017.
+ */
+public class MyValidator {
+}
---
+++ src/OOValidator.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * Created by Matt on 6/28/2017.
+ */
+public class OOValidator {
+
+    public OOValidator(Scanner sc) {
+
+    }
+
+    public int getInt(String prompt) {
+        return 0;
+    }
+
+    public int getIntWithinRange(String prompt, int min, int max){
+        return 0;
+    }
+
+    public double getDouble(String prompt){
+        return 0;
+    }
+
+    public double getDoubleWithinRange(String prompt, double min, double max){
+        return 0;
+    }
+}
