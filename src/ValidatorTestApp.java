c11f85827c53f1240a9b1923fca0ee9f89f5dec1
Matthew Menna <mattmenna@gmail.com>
2017-06-30 13:21:23 -0400

    Switch statemetn added for control

--- src/ValidatorTestApp.java
+++ src/ValidatorTestApp.java
@@ -17,35 +17,47 @@ public class ValidatorTestApp {
     public static void main(String[] args) {
         System.out.println("Validator Test App");
         int caseNumber = 0;
+        char cont='y';
 
         Scanner scnr = new Scanner(System.in);
         OOValidator validClass = new OOValidator(scnr);
-        System.out.println("What Method do you want to test?");
-
-        System.out.println("/t1: Test for .getInt method");
-        System.out.println("/t2: Test for .getIntWithinRange method");
-        System.out.println("/t3: Test for .getDouble method");
-        System.out.println("/t4: Test for .getDoubleWithinRange method");
-        switch (caseNumber) {
-            case 1: {
-                System.out.println(validClass.getInt("Enter an integer:"));
-                System.out.println();
-            }
-            case 2: {
-                System.out.println(validClass.getIntWithinRange("Enter an integer between 4 and 10", 4, 10));
-                System.out.println();
-            }
-            case 3: {
-                System.out.println(validClass.getDouble("Enter a double:"));
-                System.out.println();
-            }
-            case 4: {
-                ;
-                System.out.println(validClass.getDoubleWithinRange("Enter a double between 0 and 100", 0, 100));
-                System.out.println();
-            }
-        }
 
 
-    }
-}
+        while( cont !='n' ) {
+            System.out.println("What Method do you want to test?");
+
+            System.out.println("\t1: Test for .getInt method");
+            System.out.println("\t2: Test for .getIntWithinRange method");
+            System.out.println("\t3: Test for .getDouble method");
+            System.out.println("\t4: Test for .getDoubleWithinRange method");
+            System.out.println("Pick one: ");
+            caseNumber = scnr.nextInt();
+
+            switch (caseNumber) {
+                case 1: {
+                    System.out.println(validClass.getInt("Enter an integer:"));
+                    System.out.println();
+                    break;
+                }
+                case 2: {
+                    System.out.println(validClass.getIntWithinRange("Enter an integer between 4 and 10", 4, 10));
+                    System.out.println();
+                    break;
+                }
+                case 3: {
+                    System.out.println(validClass.getDouble("Enter a double:"));
+                    System.out.println();
+                    break;
+                }
+                case 4: {
+                    System.out.println(validClass.getDoubleWithinRange("Enter a double between 0 and 100", 0, 100));
+                    System.out.println();
+                    break;
+                }
+            } //end switch
+            System.out.println("Do you want to continue? (y/n):");
+            cont = scnr.next().charAt(0);
+
+        } // end while
+    } // end main
+} // end class
